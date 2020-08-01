72382a94c5ae76a30defa8eb75430e16c8f21afc
Thomas Dupouy <thomas.mylab33@gmail.com>
2020-08-01 08:16:22 +0200

    Initial release

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.use('/lib', express.static('lib'));
+app.use('/docs', express.static('docs'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
