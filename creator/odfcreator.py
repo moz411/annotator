889fb6a87e5fb0ecff3212fbb161a4cc8912a82e
Thomas Dupouy <moz@free.fr>
2020-10-18 21:42:15 +0200

    Able to create odt

---
+++ creator/odfcreator.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import json
+from odf.opendocument import OpenDocumentText
+from odf.style import (Style, TextProperties, ParagraphProperties,
+                       ListLevelProperties, TabStop, TabStops)
+from odf.text import (H, P, List, ListItem, ListStyle, ListLevelStyleNumber,
+                      ListLevelStyleBullet)
+from odf import teletype
+
+source_file = '../dataset/MachineLearningForDummies/extracted.json'
+destination_file = '../dataset/MachineLearningForDummies/extracted.odt'
+with open(source_file, 'r') as file:
+    extracted = json.load(file)
+
+textdoc = OpenDocumentText()
+
+# Creating different style used in the document
+s = textdoc.styles
+
+# For Level-1 Headings that are centerd
+h1style = Style(name="CenterHeading 1", family="paragraph")
+h1style.addElement(ParagraphProperties(attributes={"textalign": "center"}))
+h1style.addElement(TextProperties(attributes={"fontsize": "18pt", "fontweight": "bold"}))
+
+# For Level-2 Headings that are centered
+h2style = Style(name="CenterHeading 2", family="paragraph")
+h2style.addElement(ParagraphProperties(attributes={"textalign": "center"}))
+h2style.addElement(TextProperties(attributes={"fontsize": "15pt", "fontweight": "bold"}))
+
+# For bold text
+boldstyle = Style(name="Bold", family="text")
+boldstyle.addElement(TextProperties(attributes={"fontweight": "bold"}))
+
+# For numbered list
+numberedliststyle = ListStyle(name="NumberedList")
+level = 1
+numberedlistproperty = ListLevelStyleNumber(
+    level=str(level), numsuffix=".", startvalue=1)
+numberedlistproperty.setAttribute('numsuffix', ".")
+numberedlistproperty.addElement(ListLevelProperties(
+    minlabelwidth="%fcm" % (level - .2)))
+numberedliststyle.addElement(numberedlistproperty)
+
+# For Bulleted list
+bulletedliststyle = ListStyle(name="BulletList")
+level = 1
+bulletlistproperty = ListLevelStyleBullet(level=str(level), bulletchar=u"•")
+bulletlistproperty.addElement(ListLevelProperties(
+    minlabelwidth="%fcm" % level))
+bulletedliststyle.addElement(bulletlistproperty)
+
+
+# Justified style
+justifystyle = Style(name="justified", family="paragraph")
+justifystyle.addElement(ParagraphProperties(
+    attributes={"textalign": "justify"}))
+
+# Creating a tabstop at 10cm
+tabstops_style = TabStops()
+tabstop_style = TabStop(position="10cm")
+tabstops_style.addElement(tabstop_style)
+tabstoppar = ParagraphProperties()
+tabstoppar.addElement(tabstops_style)
+tabparagraphstyle = Style(name="Question", family="paragraph")
+tabparagraphstyle.addElement(tabstoppar)
+s.addElement(tabparagraphstyle)
+
+
+# Register created styles to styleset
+s.addElement(h1style)
+s.addElement(h2style)
+s.addElement(boldstyle)
+s.addElement(numberedliststyle)
+s.addElement(bulletedliststyle)
+s.addElement(justifystyle)
+s.addElement(tabparagraphstyle)
+
+
+for page, items in enumerate(extracted):
+    for box in items:
+        if(box['label'] == 'Titre 1'):
+            # Adding main heading
+            mymainheading_element = H(outlinelevel=1, stylename=h1style)
+            mymainheading_text = box['text']
+            teletype.addTextToElement(mymainheading_element, mymainheading_text)
+            textdoc.text.addElement(mymainheading_element)
+
+        if(box['label'] == 'Titre 2'):
+            # Adding main heading
+            mymainheading_element = H(outlinelevel=2, stylename=h2style)
+            mymainheading_text = box['text']
+            teletype.addTextToElement(mymainheading_element, mymainheading_text)
+            textdoc.text.addElement(mymainheading_element)
+
+        if(box['label'] == 'Auteur'):
+            # Adding second heading
+            mysecondheading_element = H(outlinelevel=2, stylename=h2style)
+            mysecondheading_text =  box['text']
+            teletype.addTextToElement(mysecondheading_element, mysecondheading_text)
+            textdoc.text.addElement(mysecondheading_element)
+
+        if(box['label'] == 'Paragraphe'):
+            # Adding a paragraph
+            paragraph_element = P(stylename=justifystyle)
+            paragraph_text = box['text']
+            teletype.addTextToElement(paragraph_element, paragraph_text)
+            textdoc.text.addElement(paragraph_element, paragraph_text)
+
+textdoc.save(destination_file)
--- extractor/extractor.py
+++ extractor/extractor.py
@@ -1,37 +1,131 @@
 #!/usr/bin/env python
 
+import io
 import pprint as pp
 import json
+import string
+import random
+import tempfile
+import cv2
+from PIL import Image
 from statistics import mode
 from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextContainer, LTChar
+from pdfminer.layout import LTTextContainer, LTChar, LTImage, LTRect, LTFigure, LTTextBox
+from pdf2image import convert_from_path, convert_from_bytes
 
-extract = '../dataset/MachineLearningForDummies/extract.pdf'
-annotation = '../dataset/MachineLearningForDummies/annotation.json'
-result = []
+pdf_file = '../dataset/MachineLearningForDummies/extract.pdf'
+extract_vision_file = '../dataset/MachineLearningForDummies/output-1-to-12.json'
+user_annotation_file = '../dataset/MachineLearningForDummies/annotator.json'
+extracted_file = '../dataset/MachineLearningForDummies/extracted.json'
+images_dir = '../dataset/MachineLearningForDummies/images/'
 
-for page_layout in extract_pages(extract):
-    for element in page_layout:
+""" 
+for num, page in enumerate(extract_pages(pdf_file)):
+    elements = []
+    for element in page:
         if isinstance(element, LTTextContainer):
             try:
                 fonttype = mode([c.fontname for text_line in element 
                             for c in text_line  if isinstance(c, LTChar)]).split('-')[1]
             except IndexError:
                 fonttype = 'Regular'
+            permitted = string.ascii_letters + string.punctuation + string.whitespace
+            txt = "".join(e for e in element.get_text() if e in permitted)
+            try:
+                first_word = txt.split()[0].upper()
+            except IndexError:
+                first_word = txt
+
             res = {
                 'bbox': element.bbox,
-                'first_word': element.get_text().split()[0][:20].upper(),
+                'x': element.bbox[0],
+                'y': page.height - element.bbox[3],
+                'dx': element.bbox[2] - element.bbox[0],
+                'dy': element.bbox[3] - element.bbox[1],
+                'first_word': first_word,
+                'text': txt,
                 'height': element.height,
                 'fonttype': fonttype,
-                'density': sum(1 for text_line in element for c in text_line  if isinstance(c, LTChar))
+                'density': sum(1 for text_line in element for c in text_line  if isinstance(c, LTChar)),
+                'color': 'black'
                 }
-            result.append(res)
-    break
-pp.pprint(result)
-print()
+            elements.append(res)
+        # if isinstance(element, LTFigure):
+        #     for ltimageobject in element:
+        #         if isinstance(ltimageobject, LTImage):
+        #             im = Image.frombytes(mode="1", 
+        #                                 data = ltimageobject.stream.data, 
+        #                                 size = ltimageobject.srcsize,
+        #                                 decoder_name='raw') 
+        #             im.save("new_fromIm_%s.jpg" % num)
 
-with open(annotation, 'r') as file:
-    selected = json.load(file)[0]
-    selected = [elem for elem in selected if 'label' in elem]
 
-pp.pprint(selected)
+    pdf.append(elements) """
+    
+# with open("pdf.json", "w") as file:
+#    json.dump({'responses': pdf}, file)
+
+extracted = []
+with open(extract_vision_file, 'r') as file:
+    extract_vision = json.load(file)
+
+for page in extract_vision['responses']:
+    elements = []
+    width = page['fullTextAnnotation']['pages'][0]['width']
+    height = page['fullTextAnnotation']['pages'][0]['height']
+    blocks = page['fullTextAnnotation']['pages'][0]['blocks']
+    for num, block in enumerate(blocks):          
+        res = {}
+        coords = block['boundingBox']['normalizedVertices']
+        coords = [[e['x'] * width,
+                  e['y'] * height] for e in coords]
+        res['x'] = coords[0][0]
+        res['y'] = coords[0][1]
+        res['dx'] = coords[2][0] - res['x']
+        res['dy'] = coords[2][1] - res['y']
+        texte = []
+        for word in block['paragraphs'][0]['words']:
+            for symbol in word['symbols']:
+                if ('text' in symbol.keys()):
+                    texte.append(symbol['text'])
+                if ('property' in symbol.keys()):
+                    if ('detectedBreak' in symbol['property'].keys() 
+                        and symbol['property']['detectedBreak']['type'] in ['SPACE', 'LINE_BREAK']):
+                        texte.append(' ')
+            #texte.append(' ')
+        res['text'] = ''.join(texte)
+        elements.append(res)
+    extracted.append(elements)
+
+annotated = []
+with open(user_annotation_file, 'r') as file:
+    user_annotation = json.load(file)
+
+for pagenum, page in enumerate(user_annotation):
+    blocs = [elem for elem in page if 'label' in elem]
+    elements = []
+    for blocnum, bloc in enumerate(blocs):
+        res = [elem for elem in extracted[pagenum] if 
+                            bloc['x'] <= elem['x'] and
+                            bloc['y'] <= elem['y'] and
+                            bloc['x'] + bloc['dx'] >= elem['x'] + elem['dx'] and
+                            bloc['y'] + bloc['dy']  >= elem['y'] + elem['dy']]
+        if res != []:
+            res[0].update(bloc)
+            res = res[0]
+        else:
+            res = bloc
+        elements.append(res)
+    annotated.append(elements)
+
+with open(extracted_file, "w") as file:
+    json.dump(annotated, file)
+
+# pp.pprint(annotated)
+
+# Extract all pages as images
+""" images_from_path = convert_from_path(pdf_file, 
+                                     dpi=200,
+                                     grayscale = True,
+                                     fmt='jpg',
+                                     output_folder=images_dir) """
